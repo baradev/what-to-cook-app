f3f4a09302ce242e6ea90a162a75ff031761dbea
Barbora Klusackova <barboraklusackova@gmail.com>
2024-02-13 15:41:15 +1300

    add fix input styling

--- app/components/DayLine.tsx
+++ app/components/DayLine.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useState } from 'react'
 import { HeartButton } from './HeartButton'
 import MealInput from './MealInput'
@@ -24,29 +25,38 @@ const DayLine = () => {
     setFocusedIndex((prevIndex) => (prevIndex - 1 + totalInputs) % totalInputs)
   }
 
-  const renderDayInputs = () => {
-    return daysOfWeek.map((day, index) => (
-      <div key={day} className="flex flex-row ml-10">
-        <div key={day} className="flex flex-col p-2 border-b w-96">
-          <label className="mb-4" htmlFor={`${day}DinnerInput`}>
-            {day}:
-          </label>
-          <MealInput
-            day={day}
-            index={index}
-            totalInputs={totalInputs}
-            onFocusNext={handleFocusNext}
-            onFocusPrevious={handleFocusPrevious}
-          />
-        </div>
-        <div className="p-2 border-b flex align-middle">
-          <HeartButton />
-        </div>
-      </div>
-    ))
+  const handleKeyDown = (event: React.KeyboardEvent) => {
+    if (event.key === 'ArrowDown') {
+      handleFocusNext()
+    } else if (event.key === 'ArrowUp') {
+      handleFocusPrevious()
+    }
   }
 
-  return <>{renderDayInputs()}</>
+  return (
+    <div onKeyDown={handleKeyDown} tabIndex={-1}>
+      {daysOfWeek.map((day, index) => (
+        <div key={day} className="flex flex-row ml-10">
+          <div key={day} className="flex flex-col p-2 border-b w-96">
+            <label className="mb-4" htmlFor={`${day}DinnerInput`}>
+              {day}:
+            </label>
+            <MealInput
+              day={day}
+              index={index}
+              totalInputs={totalInputs}
+              onFocusNext={handleFocusNext}
+              onFocusPrevious={handleFocusPrevious}
+              isFocused={focusedIndex === index}
+            />
+          </div>
+          <div className="p-2 border-b flex align-middle">
+            <HeartButton />
+          </div>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default DayLine
--- app/components/MealInput.tsx
+++ app/components/MealInput.tsx
@@ -4,23 +4,50 @@ import React from 'react'
 
 interface Props {
   day: string
+  index: number
+  totalInputs: number
+  onFocusNext: () => void
+  onFocusPrevious: () => void
+  isFocused: boolean // Add isFocused prop to Props interface
 }
 
-const MealInput: React.FC<Props> = ({ day }) => {
+const MealInput: React.FC<Props> = ({
+  day,
+  index,
+  totalInputs,
+  onFocusNext,
+  onFocusPrevious,
+  isFocused,
+}) => {
+  const inputRef = React.useRef<HTMLInputElement>(null)
+
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Enter') {
       event.currentTarget.blur()
+    } else if (event.key === 'ArrowDown') {
+      onFocusNext()
+    } else if (event.key === 'ArrowUp') {
+      onFocusPrevious()
     }
   }
+
+  React.useEffect(() => {
+    if (isFocused && inputRef.current) {
+      inputRef.current.focus()
+    }
+  }, [isFocused])
+
   return (
     <div>
       <input
+        ref={inputRef}
         type="text"
         id={`${day}DinnerInput`}
         name={`${day}DinnerInput`}
         placeholder={`Enter ${day}'s Dinner Idea`}
-        className=" input-accent w-full max-w-xs h-8"
+        className="w-full max-w-xs h-8 input-success "
         onKeyDown={handleKeyDown}
+        onFocus={() => inputRef.current?.select()}
       />
     </div>
   )
