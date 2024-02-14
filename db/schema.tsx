b1412ca2aed88f7d80b8f4d0eaaba1851b213031
Barbora Klusackova <barboraklusackova@gmail.com>
2024-02-14 23:36:57 +1300

    delete id column

--- db/schema.tsx
+++ db/schema.tsx
@@ -1,7 +1,6 @@
 import { integer, text, sqliteTable } from 'drizzle-orm/sqlite-core'
 
 export const meal = sqliteTable('Meal', {
-  id: integer('id'), // Keep the id field
   date: text('date'), // Add a text field to store the date
   mealName: text('mealName'), // Add mealName field
 })
