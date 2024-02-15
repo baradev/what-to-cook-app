1f208734e5cc1bd815a23640bbf2360a3df9dd00
Barbora Klusackova <barboraklusackova@gmail.com>
2024-02-15 21:40:41 +1300

    nevim co delam

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,16 @@
 import DayLine from './components/DayLine'
 
-export default function Home() {
+export default async function Home() {
+  const data: any = await db.select().from(meal)
+
+  console.log(data)
+
+  const addMealToDatabase = async (mealData: any) => {
+    'use server'
+    await db.insert(meal).values(mealData)
+    console.log('Meal added to database:', mealData)
+  }
+
   return (
     <div className="flex">
       <div className="flex flex-col border-r p-4">
